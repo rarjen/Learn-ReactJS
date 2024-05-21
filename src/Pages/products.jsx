b16cab0f25d97c37a76c5f2daa2abed97cfa7bb4
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-21 08:39:50 +0700

    update products.jsx

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,29 +1,49 @@
 import CardProduct from "../components/Fragements/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Airbus A319",
+    price: "IDR 569.000",
+    image: "/images/airbus.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio unde
+    praesentium eaque aliquam corrupti, doloribus consequatur, laudantium
+    fugiat placeat a distinctio illum qui cupiditate ipsam quis voluptas,
+    incidunt facilis! Ut.`,
+  },
+  {
+    id: 2,
+    name: "Airbus A320",
+    price: "IDR 659.000",
+    image: "/images/airbus.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio unde
+    praesentium eaque aliquam corrupti.`,
+  },
+  {
+    id: 3,
+    name: "Airbus A330",
+    price: "IDR 719.000",
+    image: "/images/airbus.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+  },
+];
+
 //Digunakan apabila ingin me-render salah 1 saja
 const ProductsPage = () => {
+  console.log(products);
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/airbus.jpg" />
-        <CardProduct.Body title="Airbus A319">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio unde
-          praesentium eaque aliquam corrupti, doloribus consequatur, laudantium
-          fugiat placeat a distinctio illum qui cupiditate ipsam quis voluptas,
-          incidunt facilis! Ut.
-        </CardProduct.Body>
-        <CardProduct.Footer price="IDR 659.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/airbus.jpg" />
-        <CardProduct.Body title="Airbus A319">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio unde
-          praesentium eaque aliquam corrupti, doloribus consequatur, laudantium
-          fugiat placeat a distinctio illum qui cupiditate ipsam quis voluptas,
-          incidunt facilis! Ut.
-        </CardProduct.Body>
-        <CardProduct.Footer price="IDR 659.000" />
-      </CardProduct>
+      {products.map((product) => {
+        return (
+          <CardProduct>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        );
+      })}
     </div>
   );
 };
