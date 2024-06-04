20743db51996431fade967d118dfa55c7a71c35c
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-06-04 09:38:41 +0700

    update Counter.jsx

--- src/components/Fragements/Counter.jsx
+++ src/components/Fragements/Counter.jsx
@@ -7,7 +7,21 @@ class Counter extends React.Component {
     this.state = {
       count: 0,
     };
+    console.log("constructor");
   }
+
+  componentDidMount() {
+    this.setState({ count: 1 });
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+    if (this.state.count === 10) {
+      this.setState({ count: 0 });
+    }
+  }
+
   render() {
     return (
       <div className="flex items-center">
@@ -18,6 +32,7 @@ class Counter extends React.Component {
         >
           +
         </button>
+        {console.log("render")}
       </div>
     );
   }
