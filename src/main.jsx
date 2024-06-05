1727c85b7a5644288e6e7cabd4eea42539b475c8
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-06-05 12:30:15 +0700

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import RegisterPage from "./Pages/register.jsx";
 import ErrorPage from "./Pages/errorPage.jsx";
 import ProductsPage from "./Pages/products.jsx";
 import ProfilePage from "./Pages/profile.jsx";
+import DetailProductPage from "./Pages/detailProduct.jsx";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
   {
     path: "/profile",
     element: <ProfilePage />,
+  },
+  {
+    path: "/product/:id",
+    element: <DetailProductPage />,
   },
 ]);
 
